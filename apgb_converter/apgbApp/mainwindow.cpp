735b6977ed33f82845542db5dcc990087c285d96
KofiAnnan97 <kofi4annan@gmail.com>
2025-04-07 04:34:20 -0400

    Introduced saving palettes  and importing functionality for csv files

--- apgb_converter/apgbApp/mainwindow.cpp
+++ apgb_converter/apgbApp/mainwindow.cpp
@@ -5,8 +5,11 @@
 #include <QString>
 #include <QDebug>
 #include <QDialog>
+#include <QFileDialog>
+#include <fstream>
+#include <vector>
 
-using namespace std; 
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,6 +18,9 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     QString darkest = "#000000";
     QString brightest = "#FFFFFF";
+
+    footer = "8141504742";
+    lcdOff = "FFFFFF";
 
     // Set text fields for darkest and brightest hex values on startup
     ui->txt_bg_0->setText(darkest);
@@ -54,8 +60,241 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_btn_gen_file_clicked()
+int* MainWindow::fromStrToHexStrArr(string str, int numOfBytes){
+    int *hexArr = new int[numOfBytes];
+    //if(strcmp(str.substr(0, 2).c_str(), "0x") == 0) str.erase(str.begin(), str.begin()+2);
+    for(int i = 0; i < numOfBytes;++i){
+      string segment = str.substr(i*2, 2);
+      stringstream ss;
+      ss << std::hex << segment;
+      ss >> hexArr[i];
+    }
+    return hexArr;
+}
+
+string MainWindow::fromHexStrToStr(string hexStr, int numOfBtyes){
+    //string test = hexStr.replace(0, "\\x", ",");
+    //qDebug() << "Hex string: " << test;
+    string str = "";
+    for(auto c : hexStr){
+        qDebug() << c;
+        //if((c >= '0' && c <= '9') || (c >='a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+            str += c;
+        //}
+    }
+    return str;
+}
+
+string MainWindow::trim(string data){
+    int startIdx = 0;
+    int endIdx = data.length()-1;
+    while(data[startIdx] == ' ') startIdx++;
+    while(data[endIdx] == ' ') endIdx--;
+    return data.substr(startIdx, endIdx-startIdx+1);
+}
+
+void MainWindow::clearBuffer(char *buffer, int size){
+    for(int j = 0; j < size; j++) buffer[j] = 0x00;
+}
+
+bool MainWindow::isKeyword(string word){
+    vector<string> keywords = {"BG", "OBJ0", "OBJ1", "WINDOW", "bg", "obj0", "obj1", "window"};
+    for(int i = 0; i < keywords.size(); i++){
+        if(word == keywords[i]) return true;
+    }
+    return false;
+}
+
+APGB_Palette MainWindow::importPaletteFromCSV(string filename){
+    APGB_Palette ap;
+    ap.bg = new string[4];
+    ap.obj0 = new string[4];
+    ap.obj1 = new string[4];
+    ap.window = new string[4];
+    fstream csv;
+    csv.open(filename, ios::in);
+    if(csv.is_open()){
+        vector<string> row;
+        string line, word, temp;
+        while(csv >> temp){
+            row.clear();
+            getline(csv, line);
+            stringstream s(line);
+            while (getline(s, word, ',')){
+                string trimmedWord = this->trim(word);
+                if(!this->isKeyword(trimmedWord) && trimmedWord.substr(0, 2) == "0x"){
+                    trimmedWord.erase(0, 2);
+                    trimmedWord = "#" + trimmedWord;
+                }else if(!this->isKeyword(trimmedWord) && trimmedWord[0] != '#' && trimmedWord.size() == 6){
+                    trimmedWord = "#" + trimmedWord;
+                }
+                row.push_back(trimmedWord);
+                //qDebug() << trimmedWord;
+            }
+            string *pt;
+            auto pType = row[0].c_str();
+            if(strcmp(pType , "BG") == 0 || strcmp(pType ,"bg") == 0)              pt = ap.bg;
+            else if(strcmp(pType , "OBJ0") == 0 || strcmp(pType ,"obj0") == 0)     pt = ap.obj0;
+            else if(strcmp(pType , "OBJ1") == 0 || strcmp(pType ,"obj1") == 0)     pt = ap.obj1;
+            else if(strcmp(pType , "WINDOW") == 0 || strcmp(pType ,"window") == 0) pt = ap.window;
+            pt[0] = row[1];
+            pt[1] = row[2];
+            pt[2] = row[3];
+            pt[3] = row[4];
+        }
+    }
+    else qDebug() << "Failed to find file: " << filename;
+    return ap;
+}
+
+APGB_Palette MainWindow::importPaletteFromPAL(string filename){
+    APGB_Palette ap;
+    ap.bg = new string[4];
+    ap.obj0 = new string[4];
+    ap.obj1 = new string[4];
+    ap.window = new string[4];
+    fstream pal;
+    pal.open(filename, ios::in);
+    if(pal.is_open()){
+        string line, word, temp;
+        while(pal >> temp){
+            //row.clear();
+            //qDebug() << temp;
+            //getline(pal, line);
+            stringstream ss(temp);
+            //ss << std::hex << temp;
+            ss >> line;
+            qDebug() << line << " " << line.size();
+            if(line.size() == 56){
+                line.erase(51,5);
+                ap.bg[0] = "#"+this->fromHexStrToStr(line.substr(0, 3), 6);
+                qDebug() << ap.bg[0];
+                ap.bg[1] = "#"+line.substr(3, 3);
+                ap.bg[2] = "#"+line.substr(6, 3);
+                ap.bg[3] = "#"+line.substr(9, 3);
+                ap.obj0[0] = "#"+line.substr(12,3);
+                ap.obj0[1] = "#"+line.substr(15,3);
+                ap.obj0[2] = "#"+line.substr(18,3);
+                ap.obj0[3] = "#"+line.substr(21,3);
+                ap.obj1[0] = "#"+line.substr(24,3);
+                ap.obj1[1] = "#"+line.substr(27,3);
+                ap.obj1[2] = "#"+line.substr(30,3);
+                ap.obj1[3] = "#"+line.substr(33,3);
+                ap.window[0] = "#"+line.substr(36,3);
+                ap.window[1] = "#"+line.substr(39,3);
+                ap.window[2] = "#"+line.substr(42,3);
+                ap.window[3] = "#"+line.substr(45,3);
+                break;
+            }
+        }
+    }
+    else qDebug() << "Failed to find file: " << filename;
+    return ap;
+}
+
+char* MainWindow::APGBFormat(APGB_Palette p, int size){
+    string data = "";
+    for(int a = 0; a < 4; a++){
+      if(strcmp(p.bg[a].substr(0, 2).c_str(), "0x") == 0){
+        p.bg[a].erase(p.bg[a].begin(), p.bg[a].begin()+2);
+      }
+      data += p.bg[a];
+    } 
+    for(int c = 0; c < 4; c++){
+      if(strcmp(p.obj0[c].substr(0, 2).c_str(), "0x") == 0){
+        p.obj0[c].erase(p.obj0[c].begin(), p.obj0[c].begin()+2);
+      }
+      data += p.obj0[c];
+    } 
+    for(int e = 0; e < 4; e++){
+      if(strcmp(p.obj1[e].substr(0, 2).c_str(), "0x") == 0){
+        p.obj1[e].erase(p.obj1[e].begin(), p.obj1[e].begin()+2);
+      }
+      data += p.obj1[e];
+    }
+    for(int g = 0; g < 4; g++) {
+      if(strcmp(p.window[g].substr(0, 2).c_str(), "0x") == 0){
+        p.window[g].erase(p.window[g].begin(), p.window[g].begin()+2);
+      }
+      data += p.window[g];
+    }
+    data += lcdOff + footer;
+    qDebug() << data;
+
+    int *temp = fromStrToHexStrArr(data, size);
+    char *buffer = new char[size];
+    clearBuffer(buffer, size);
+    for(int i = 0; i < size; i++) buffer[i] = temp[i];
+    return buffer;
+} 
+
+APGB_Palette MainWindow::getPalette(string *bg, string *obj0, string *obj1, string *window){
+    APGB_Palette pal;
+    pal.bg = bg;
+    pal.obj0 = obj0;
+    pal.obj1 = obj1;
+    pal.window = window;
+    return pal;
+}
+
+void MainWindow::savePalette(string filename, APGB_Palette p){
+    std::ofstream df(filename, std::ios_base::binary | std::ios_base::out);
+    if(df.is_open()){
+        char *buffer = this->APGBFormat(p, p.numOfBytes);
+        df.write(buffer, p.numOfBytes);
+        df.close();
+    }
+    else qDebug() << "Failed to create file: " << filename;
+}
+
+void MainWindow::on_btn_import_clicked()
 {
+    QString filename = QFileDialog::getOpenFileName(this, tr("Import"), tr(""));
+    qDebug() << "Opening file: " << filename;
+    APGB_Palette p;
+    if(filename.endsWith(".csv")){
+        p = this->importPaletteFromCSV(filename.toStdString());
+    }
+    else if(filename.endsWith(".pal")){
+        p = this->importPaletteFromPAL(filename.toStdString());
+    }
+    ui->txt_bg_0->setText(QString::fromStdString(p.bg[0]));
+    ui->btn_bg_0->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.bg[0]) + "}");
+    /*ui->txt_bg_1->setText(QString::fromStdString(p.bg[1]));
+    ui->btn_bg_1->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.bg[1]) + "}");
+    ui->txt_bg_2->setText(QString::fromStdString(p.bg[2]));
+    ui->btn_bg_2->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.bg[2]) + "}");
+    ui->txt_bg_3->setText(QString::fromStdString(p.bg[3]));
+    ui->btn_bg_3->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.bg[3]) + "}");
+    ui->txt_obj0_0->setText(QString::fromStdString(p.obj0[0]));
+    ui->btn_obj0_0->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj0[0]) + "}");
+    ui->txt_obj0_1->setText(QString::fromStdString(p.obj0[1]));
+    ui->btn_obj0_1->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj0[1]) + "}");
+    ui->txt_obj0_2->setText(QString::fromStdString(p.obj0[2]));
+    ui->btn_obj0_2->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj0[2]) + "}");
+    ui->txt_obj0_3->setText(QString::fromStdString(p.obj0[3]));
+    ui->btn_obj0_3->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj0[3]) + "}");
+    ui->txt_obj1_0->setText(QString::fromStdString(p.obj1[0]));
+    ui->btn_obj1_0->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj1[0]) + "}");
+    ui->txt_obj1_1->setText(QString::fromStdString(p.obj1[1]));
+    ui->btn_obj1_1->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj1[1]) + "}");
+    ui->txt_obj1_2->setText(QString::fromStdString(p.obj1[2]));
+    ui->btn_obj1_2->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj1[2]) + "}");
+    ui->txt_obj1_3->setText(QString::fromStdString(p.obj1[3]));
+    ui->btn_obj1_3->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.obj1[3]) + "}");
+    ui->txt_window_0->setText(QString::fromStdString(p.window[0]));
+    ui->btn_window_0->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.window[0]) + "}");
+    ui->txt_window_1->setText(QString::fromStdString(p.window[1]));
+    ui->btn_window_1->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.window[1]) + "}");
+    ui->txt_window_2->setText(QString::fromStdString(p.window[2]));
+    ui->btn_window_2->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.window[2]) + "}");
+    ui->txt_window_3->setText(QString::fromStdString(p.window[3]));
+    ui->btn_window_3->setStyleSheet("QPushButton { background-color : " + QString::fromStdString(p.window[3]) + "}");*/
+}
+
+void MainWindow::on_btn_save_clicked()
+{
+    QString errorMsg;
     QString bg_0 = ui->txt_bg_0->toPlainText();
     QString bg_1 = ui->txt_bg_1->toPlainText();
     QString bg_2 = ui->txt_bg_2->toPlainText();
@@ -73,31 +312,56 @@ void MainWindow::on_btn_gen_file_clicked()
     QString window_2 = ui->txt_window_2->toPlainText();
     QString window_3 = ui->txt_window_3->toPlainText();
 
+    if(bg_0.isEmpty()) errorMsg += "bg_0\n";
+    else bg_0.remove(0,1);
+    if(bg_1.isEmpty()) errorMsg += "bg_1\n";
+    else bg_1.remove(0,1);
+    if(bg_2.isEmpty()) errorMsg += "bg_2\n";
+    else bg_2.remove(0,1);
+    if(bg_3.isEmpty()) errorMsg += "bg_3\n";
+    else bg_3.remove(0,1);
+    if(obj0_0.isEmpty()) errorMsg += "obj0_0\n";
+    else obj0_0.remove(0,1);
+    if(obj0_1.isEmpty()) errorMsg += "obj0_1\n";
+    else obj0_1.remove(0,1);
+    if(obj0_2.isEmpty()) errorMsg += "obj0_2\n";
+    else obj0_2.remove(0,1);
+    if(obj0_3.isEmpty()) errorMsg += "obj0_3\n";
+    else obj0_3.remove(0,1);
+    if(obj1_0.isEmpty()) errorMsg += "obj1_0\n";
+    else obj1_0.remove(0,1);
+    if(obj1_1.isEmpty()) errorMsg += "obj1_1\n";
+    else obj1_1.remove(0,1);
+    if(obj1_2.isEmpty()) errorMsg += "obj1_2\n";
+    else obj1_2.remove(0,1);
+    if(obj1_3.isEmpty()) errorMsg += "obj1_3\n";
+    else obj1_3.remove(0,1);
+    if(window_0.isEmpty()) errorMsg += "window_0\n";
+    else window_0.remove(0,1);
+    if(window_1.isEmpty()) errorMsg += "window_1\n";
+    else window_1.remove(0,1);
+    if(window_2.isEmpty()) errorMsg += "window_2\n";
+    else window_2.remove(0,1);
+    if(window_3.isEmpty()) errorMsg += "window_3\n";
+    else window_3.remove(0,1);
+
     qDebug() << "Retrieved data: \n" << bg_0 << " " << bg_1 << " " << bg_2 << " " << bg_3;
     qDebug() << obj0_0 << " " << obj0_1 << " " << obj0_2 << " " << obj0_3;
     qDebug() << obj1_0 << " " << obj1_1 << " " << obj1_2 << " " << obj1_3;
     qDebug() << window_0 << " " << window_1 << " " << window_2 << " " << window_3;
+    if(!errorMsg.isEmpty()){
+        errorMsg = "The following values are empty:\n" + errorMsg;
+        qDebug() << errorMsg;
+    }else{
+        string *bg = new string[4]{bg_0.toStdString(), bg_1.toStdString(), bg_2.toStdString(), bg_3.toStdString()};
+        string *obj0 = new string[4]{obj0_0.toStdString(),obj0_1.toStdString(),obj0_2.toStdString(),obj0_3.toStdString()};
+        string *obj1 = new string[4]{obj1_0.toStdString(),obj1_1.toStdString(),obj1_2.toStdString(),obj1_3.toStdString()};
+        string *window = new string[4]{window_0.toStdString(), window_1.toStdString(), window_2.toStdString(), window_3.toStdString()};
+        APGB_Palette p = this->getPalette(bg, obj0, obj1, window);
 
-    QString errorMsg = "The following values are empty:\n";
-    if(bg_0.isEmpty()) errorMsg += "bg_0\n";
-    if(bg_1.isEmpty()) errorMsg += "bg_1\n";
-    if(bg_2.isEmpty()) errorMsg += "bg_2\n";
-    if(bg_3.isEmpty()) errorMsg += "bg_3\n";
-    if(obj0_0.isEmpty()) errorMsg += "obj0_0\n";
-    if(obj0_1.isEmpty()) errorMsg += "obj0_1\n";
-    if(obj0_2.isEmpty()) errorMsg += "obj0_2\n";
-    if(obj0_3.isEmpty()) errorMsg += "obj0_3\n";
-    if(obj1_0.isEmpty()) errorMsg += "obj1_0\n";
-    if(obj1_1.isEmpty()) errorMsg += "obj1_1\n";
-    if(obj1_2.isEmpty()) errorMsg += "obj1_2\n";
-    if(obj1_3.isEmpty()) errorMsg += "obj1_3\n";
-    if(window_0.isEmpty()) errorMsg += "window_0\n";
-    if(window_1.isEmpty()) errorMsg += "window_1\n";
-    if(window_2.isEmpty()) errorMsg += "window_2\n";
-    if(window_3.isEmpty()) errorMsg += "obj0_3\n";
-    qDebug() << errorMsg;
-
-    //bg_0.remove(0, 1);
+        QString filename = QFileDialog::getSaveFileName(this, tr("Save GB Palette"), tr("Analogue Pocket GB Palette (*.pal)"));
+        this->savePalette(filename.toStdString(), p);
+    }
  }
 
 void MainWindow::on_btn_bg_0_clicked()
@@ -128,6 +392,38 @@ void MainWindow::on_btn_bg_3_clicked()
     ui->btn_bg_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
 }
 
+void MainWindow::on_txt_bg_0_textChanged()
+{
+    QString colorData = ui->txt_bg_0->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_bg_0->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_bg_1_textChanged()
+{
+    QString colorData = ui->txt_bg_1->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_bg_1->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_bg_2_textChanged()
+{
+    QString colorData = ui->txt_bg_2->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_bg_2->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_bg_3_textChanged()
+{
+    QString colorData = ui->txt_bg_3->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_bg_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
 void MainWindow::on_btn_obj0_0_clicked()
 {
     QString colorData = this->gethexColor();
@@ -154,6 +450,38 @@ void MainWindow::on_btn_obj0_3_clicked()
     QString colorData = this->gethexColor();
     ui->txt_obj0_3->setText(colorData);
     ui->btn_obj0_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+}
+
+void MainWindow::on_txt_obj0_0_textChanged()
+{
+    QString colorData = ui->txt_obj0_0->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj0_0->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_obj0_1_textChanged()
+{
+    QString colorData = ui->txt_obj0_1->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj0_1->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_obj0_2_textChanged()
+{
+    QString colorData = ui->txt_obj0_2->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj0_2->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_obj0_3_textChanged()
+{
+    QString colorData = ui->txt_obj0_3->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj0_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
 }
 
 void MainWindow::on_btn_obj1_0_clicked()
@@ -184,6 +512,38 @@ void MainWindow::on_btn_obj1_3_clicked()
     ui->btn_obj1_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
 }
 
+void MainWindow::on_txt_obj1_0_textChanged()
+{
+    QString colorData = ui->txt_obj1_0->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj1_0->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_obj1_1_textChanged()
+{
+    QString colorData = ui->txt_obj1_1->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj1_1->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_obj1_2_textChanged()
+{
+    QString colorData = ui->txt_obj1_2->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj1_2->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_obj1_3_textChanged()
+{
+    QString colorData = ui->txt_obj1_3->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_obj1_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
 void MainWindow::on_btn_window_0_clicked()
 {
     QString colorData = this->gethexColor();
@@ -210,4 +570,36 @@ void MainWindow::on_btn_window_3_clicked()
     QString colorData = this->gethexColor();
     ui->txt_window_3->setText(colorData);
     ui->btn_window_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+}
+
+void MainWindow::on_txt_window_0_textChanged()
+{
+    QString colorData = ui->txt_window_0->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_window_0->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_window_1_textChanged()
+{
+    QString colorData = ui->txt_window_1->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_window_1->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_window_2_textChanged()
+{
+    QString colorData = ui->txt_window_2->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_window_2->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
+}
+
+void MainWindow::on_txt_window_3_textChanged()
+{
+    QString colorData = ui->txt_window_3->toPlainText();
+    if(QColor::isValidColorName(colorData)){
+        ui->btn_window_3->setStyleSheet("QPushButton { background-color : " + colorData + "}");
+    }
 }
--- apgb_converter/apgbApp/mainwindow.h
+++ apgb_converter/apgbApp/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QString>>
+#include <QString>
 #include <QColor>
 
 QT_BEGIN_NAMESPACE
@@ -10,6 +10,13 @@ namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+using namespace std;
+
+struct APGB_Palette{
+    string *bg, *obj0, *obj1, *window;
+    int numOfBytes = 56;
+};
 
 class MainWindow : public QMainWindow
 {
@@ -19,10 +26,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     QString gethexColor();
+    APGB_Palette importPaletteFromCSV(string filename);
+    APGB_Palette importPaletteFromPAL(string filename);
+    APGB_Palette getPalette(string *bg, string *obj0, string *obj1, string *window);
+    char* APGBFormat(APGB_Palette p, int size);
+    void savePalette(string filename, APGB_Palette p);
 
+    int* fromStrToHexStrArr(string str, int numOfBytes);
+    string fromHexStrToStr(string hexStr, int numOfBtyes);
+    string trim(string data);
+    void clearBuffer(char *buffer, int size);
+    bool isKeyword(string word);
+
+    string footer, lcdOff;
 
 private slots:
-    void on_btn_gen_file_clicked();
+    void on_btn_import_clicked();
+    void on_btn_save_clicked();
+
     void on_btn_bg_0_clicked();
     void on_btn_bg_1_clicked();
     void on_btn_bg_2_clicked();
@@ -39,6 +60,23 @@ private slots:
     void on_btn_window_1_clicked();
     void on_btn_window_2_clicked();
     void on_btn_window_3_clicked();
+
+    void on_txt_bg_0_textChanged();
+    void on_txt_bg_1_textChanged();
+    void on_txt_bg_2_textChanged();
+    void on_txt_bg_3_textChanged();
+    void on_txt_obj0_0_textChanged();
+    void on_txt_obj0_1_textChanged();
+    void on_txt_obj0_2_textChanged();
+    void on_txt_obj0_3_textChanged();
+    void on_txt_obj1_0_textChanged();
+    void on_txt_obj1_1_textChanged();
+    void on_txt_obj1_2_textChanged();
+    void on_txt_obj1_3_textChanged();
+    void on_txt_window_0_textChanged();
+    void on_txt_window_1_textChanged();
+    void on_txt_window_2_textChanged();
+    void on_txt_window_3_textChanged();
 
 private:
     Ui::MainWindow *ui;
