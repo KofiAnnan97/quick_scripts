73a4272324ccd4d35bb222efdc39cc4f0f3b38b3
KofiAnnan97 <kofi4annan@gmail.com>
2025-05-01 10:24:36 -0400

    updated bash script name

--- Recreation/steam_sales_scrapper/send_email.sh
+++ Recreation/steam_sales_scrapper/send_email.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Go to script directory
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
+
+# Run script to get game sales
 cargo run -- --send-email
--- Recreation/steam_sales_scrapper/set_cron.sh
+++ Recreation/steam_sales_scrapper/set_cron.sh
@@ -3,7 +3,7 @@
 # This bash script uses /etc/cron.d to store the CRON job
 
 CRON_DIR="/etc/cron.d"
-CRON_FILE="steam_sales_scrapper"
+CRON_FILE="steam-sales-scrapper"
 
 # CRON file for automation if it does not exist
 if [ ! -f "$CRON_DIR/$CRON_FILE" ]; then
@@ -15,5 +15,5 @@ SCHEDULE="0 13 * * 2,4"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Configure as a system wide cron job
-sudo echo "$SCHEDULE root $SCRIPT_DIR/run_scrapper.sh" > "$CRON_DIR/$CRON_FILE"
+sudo echo "$SCHEDULE root $SCRIPT_DIR/send_email.sh" > "$CRON_DIR/$CRON_FILE"
 sudo chmod 600 "$CRON_DIR/$CRON_FILE"
