d97ecb4eca66b1f1cb550d0f070b09dd1a8114e9
KofiAnnan97 <kofi4annan@gmail.com>
2025-05-12 09:14:50 -0400

    Updated Windows script to use release build of script

--- Recreation/game_sales_scrapper/send_email.ps1
+++ Recreation/game_sales_scrapper/send_email.ps1
@@ -7,6 +7,12 @@ if(-not(Test-Path -path $LogFolder) -and (Test-Path $LogFolder -IsValid)){
     New-Item -Path $LogFolder -ItemType Directory
 }
 
+# Check if target directory exists
+$TargetFolder = "$PSScriptRoot\target"
+if(-not(Test-Path -path $TargetFolder) -and (Test-Path $TargetFolder -IsValid)){
+    cargo build --release
+}
+
 # Run script to get game sales
 $DateTime = (Get-Date -Format "yyyy_MM_dd_HH_mm")
-cargo run -- --send-email| Out-String | Set-Content $LogFolder\$DateTime"_email.log"
+.\target\release\game_sales_scrapper.exe --send-email | Out-String | Set-Content $LogFolder\$DateTime"_email.log"
--- Recreation/game_sales_scrapper/set_task_scheduler.ps1
+++ Recreation/game_sales_scrapper/set_task_scheduler.ps1
@@ -5,7 +5,7 @@ $action = New-ScheduledTaskAction `
     -Argument "-NoProfile -ExecutionPolicy Bypass -File $PSScriptRoot\send_email.ps1" 
 $description = "Runs a script to see if any games have reached a desired price. If yes, an email is sent."
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-Register-ScheduledTask -TaskName 'steam-sales-scrapper' `
+Register-ScheduledTask -TaskName 'game-sales-scrapper' `
     -Action $action `
     -Trigger $trigger `
     -Description $description `
